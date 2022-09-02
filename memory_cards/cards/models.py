5eeacea037734e52ad041d91c4e32f0288f1b29a
maurycywoznica <mjw11@wp.pl>
2022-09-02 22:15:37 +0200

    New sass structure

--- memory_cards/cards/models.py
+++ memory_cards/cards/models.py
@@ -7,17 +7,25 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
 
+    def __str__(self):
+        return f"{self.name}"
 
 class Card(models.Model):
     question = models.CharField(max_length=255)
     response = models.TextField(max_length=2000)
     categories = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return f"{self.question}"
 
 '''
 class UserCard(Card, Timestamped):
