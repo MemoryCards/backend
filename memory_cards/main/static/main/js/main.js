03204d9b5068513304a1df08071cfb3833a56e0b
maurycywoznica <mjw11@wp.pl>
2022-11-12 19:57:16 +0100

    new navigation

--- memory_cards/main/static/main/js/main.js
+++ memory_cards/main/static/main/js/main.js
@@ -1,3 +1,10 @@
 const footerYearSpan = document.querySelector('.y')
 const year = new Date().getFullYear();
 footerYearSpan.textContent = `${year}`
+
+const burgerBtn = document.querySelector('.burger-btn')
+const navMobile = document.querySelector('.nav-mobile')
+
+const mobileNav
+
+burgerBtn.addEventListener('click', )
