4f62fef8fb84ef0c2025de6c4551e3de891eb0af
maurycywoznica <mjw11@wp.pl>
2022-11-13 20:03:14 +0100

    new navigation

--- memory_cards/main/static/main/js/main.js
+++ memory_cards/main/static/main/js/main.js
@@ -5,6 +5,26 @@ footerYearSpan.textContent = `${year}`
 const burgerBtn = document.querySelector('.burger-btn')
 const navMobile = document.querySelector('.nav-mobile')
 
-const mobileNav
+
 
 burgerBtn.addEventListener('click', )
+
+
+//CARDS
+const search = document.querySelector('.search-cards')
+
+const li = document.querySelectorAll('.card-item')
+
+const searchDrinks = (e) =>{
+    const text = e.target.value.toLowerCase();
+
+    li.forEach(el => {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1){
+            el.style.display = 'block'
+        } else {
+            el.style.display = 'none'
+        }
+    })
+}
+
+search.addEventListener('keyup', searchDrinks)
