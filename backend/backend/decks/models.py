c821efc2ed667a80cb74e7c1f756a3fccdca95e4
maurycywoznica <mjw11@wp.pl>
2022-12-13 18:52:56 +0100

    new models

--- backend/backend/decks/models.py
+++ backend/backend/decks/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+    description = models.CharField(max_length=256)
+
+
 class Deck(models.Model):
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=256)
+    category_id = models.ForeignKey(Category.pk())
