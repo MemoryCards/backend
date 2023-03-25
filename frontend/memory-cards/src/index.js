a8294f2844f56be0a6b69cca2e976d4f9bd3a802
maurycywoznica <mjw11@wp.pl>
2023-03-25 23:24:31 +0100

    new backend

--- frontend/memory-cards/src/index.js
+++ frontend/memory-cards/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {Router, BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+router = Router
+
+
+
 root.render(
   <React.StrictMode>
     <App />
